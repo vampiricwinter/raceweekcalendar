7b1be191e0b98fd6f9a3c97f63565175468f03d9
vampiricwinter <vampiricwinter@outlook.com>
2026-02-24 15:01:23 -0300

    Add script and variables

---
+++ script.js
@@ -0,0 +1,30 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function load() {
+  const dt = new Date();
+
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const dateString = firstDayOfMonth.toLocaleDateString('en-gb', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric'
+  });
+  
+  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+  
+}
+
+
+load();
